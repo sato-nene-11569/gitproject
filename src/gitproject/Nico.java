f5ecb25f8cf257c078829f2243fbe96f2a90cede
student <student@FJ-1025>
2026-05-15 12:12:34 +0900

    Javaファイル登録★

---
+++ src/gitproject/Nico.java
@@ -0,0 +1,11 @@
+
+package gitproject;
+
+public class Nico {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/gitproject/Test.java
@@ -0,0 +1,6 @@
+package gitproject;
+
+public class Test {
+	public static void main(String[] args) {
+	}
+}
